44ed5b7df21c575b2e33c767607193d34f10c76c
Tie_Guo <117576846+Tie-Guo@users.noreply.github.com>
2023-08-12 10:03:13 +0800

    Add files via upload

--- source/PauseSubState.hx
+++ source/PauseSubState.hx
@@ -146,6 +146,10 @@ class PauseSubState extends MusicBeatSubstate
 
 		regenMenu();
 		cameras = [FlxG.cameras.list[FlxG.cameras.list.length - 1]];
+		
+		#if android
+		addVirtualPad(UP_DOWN, A);
+		#end
 	}
 
 	var holdTime:Float = 0;
